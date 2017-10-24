163fb546f36f4cf531b57761f42d74eb608d493c
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-10-24 17:33:55 +0200

    Completato esercizio

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "bool.h"
 #include "dimOf.h"
+
+/**
+ * Funzione che conta i valori maggiore del loro successivo
+ * @param array     Array
+ * @param dim       Dimensione dell'array
+ * @return          Numero elementi maggiori del loro successivo
+ */
+int countNextGreater(int *array, int dim);
 
 int main(int argc, char** argv) {
 
+    int array[] = {4, 3, 5, 2, 9};
+
+    printf("I valaori maggiori del successivo sono: %d.\n", countNextGreater(array, sizeOfArray(array)));
+
     return (EXIT_SUCCESS);
+}
+
+int countNextGreater(int *array, int dim) {
+    int count = 0;
+
+    for (int i = 0; i < dim - 1; i++) {
+        if (*(array + i) > *(array + i + 1))
+            count++;
+    }
+
+    return count;
 }
 
